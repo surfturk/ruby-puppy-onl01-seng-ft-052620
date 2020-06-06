af6a003e427f147ed264e3570c377eb93e4b8d6b
Berkley Brown <berkleydeane@gmail.com>
2020-06-06 15:26:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @save = save
+  end 
+  
+  def self.all
+    @@all = all
+  end  
+  
+  def print_all
+    @@all.each do |name|
+     puts name
+  end  
+ end  
+  
+  def save
+    @@all << self
+  end  
+  
+  
+  def self.clear_all
+    @@all.clear()
+  end  
+  
+  
+  
+end  
